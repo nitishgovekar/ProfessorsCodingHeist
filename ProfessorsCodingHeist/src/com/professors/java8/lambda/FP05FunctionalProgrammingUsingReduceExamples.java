5a61b09b589e0aaf8d945c0d01b27e9663c35798
nitish.govekar <nitishgovekar1992@gmail.com>
2021-10-07 20:11:41 +0530

    Updated code practice Functional Programming using reduce operation

---
+++ ProfessorsCodingHeist/src/com/professors/java8/lambda/FP05FunctionalProgrammingUsingReduceExamples.java
@@ -0,0 +1,24 @@
+package com.professors.java8.lambda;
+
+import java.util.List;
+
+public class FP05FunctionalProgrammingUsingReduceExamples {
+	public static void main(String[] args) {
+
+		List<Integer> numbers = List.of(10,20,7,9,8,5,100);
+		performReduceOperations(numbers);
+
+	}
+	private static void performReduceOperations(List<Integer> numbers) {
+
+		int aggregate = numbers.stream().reduce(0, (num1, num2) -> num1+num2);
+		//Here the first parameter in the reduce will be the MIN Value that an integer can have
+		int largestNumber = numbers.stream().reduce(Integer.MIN_VALUE, (num1, num2) -> num1 > num2 ? num1:num2);
+		//Here the first parameter in the reduce will be the MAX Value that an integer can have
+		int  smallestNumber = numbers.stream().reduce(Integer.MAX_VALUE, (num1, num2)-> num1 < num2 ? num1:num2);
+
+		System.out.println("Aggregate of the list is: "+ aggregate);
+		System.out.println("The largestNumber in the list is "+ largestNumber);
+		System.out.println("The smallestNumber in the list is "+ smallestNumber);
+	}
+}
