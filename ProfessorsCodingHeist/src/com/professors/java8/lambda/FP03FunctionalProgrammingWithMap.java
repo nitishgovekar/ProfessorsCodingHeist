8cc2181e05d15c0ea85057ebffd0e44af3fb54fa
nitish.govekar <nitishgovekar1992@gmail.com>
2021-10-07 14:43:58 +0530

    Updated code to demonstrate use of map in functional programming

---
+++ ProfessorsCodingHeist/src/com/professors/java8/lambda/FP03FunctionalProgrammingWithMap.java
@@ -0,0 +1,31 @@
+package com.professors.java8.lambda;
+
+import java.util.List;
+
+public class FP03FunctionalProgrammingWithMap {
+
+
+	public static void main(String[] args) {
+		List<String> courses = List.of("Spring Boot", "SpringMVC", "API", "Microservices", "Docker");
+		List<Integer> numbers = List.of(10,20,7,9,8,5,100);
+		printNumberOfCharactersInCourses(courses);
+		printCubesOfOddNumbers(numbers);
+
+	}
+
+	private static void printNumberOfCharactersInCourses(List<String> courses) {
+
+		courses.stream()
+		.map(course -> course + " " + course.length())
+		.forEach(System.out::println);
+	}
+
+	private static void printCubesOfOddNumbers(List<Integer> numbers) {
+		numbers.stream()
+		.filter(number -> number%2 != 0)
+		.map(number -> number*number*number)
+		.forEach(System.out::println);
+	}
+
+
+}
