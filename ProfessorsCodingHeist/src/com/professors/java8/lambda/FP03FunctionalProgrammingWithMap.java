0a2a30549589d27a395c517dfd5da5d1032ec4ab
nitish.govekar <nitishgovekar1992@gmail.com>
2021-10-08 18:17:54 +0530

    Updated code add new function that multiplies each number of the list by 2

--- ProfessorsCodingHeist/src/com/professors/java8/lambda/FP03FunctionalProgrammingWithMap.java
+++ ProfessorsCodingHeist/src/com/professors/java8/lambda/FP03FunctionalProgrammingWithMap.java
@@ -10,7 +10,13 @@ public class FP03FunctionalProgrammingWithMap {
 		List<Integer> numbers = List.of(10,20,7,9,8,5,100);
 		printNumberOfCharactersInCourses(courses);
 		printCubesOfOddNumbers(numbers);
+		multiplyEachNumberBy2(numbers);
 
+	}
+
+	private static void multiplyEachNumberBy2(List<Integer> numbers) {
+		System.out.println("Multiply each number by 2");
+		numbers.stream().map(number -> number * 2 ).forEach(System.out::println);
 	}
 
 	private static void printNumberOfCharactersInCourses(List<String> courses) {
