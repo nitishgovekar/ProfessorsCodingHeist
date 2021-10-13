eeac46d24d119e5b818bbd673c905d059711249c
nitish.govekar <nitishgovekar1992@gmail.com>
2021-10-13 20:23:12 +0530

    Remove unused imports

--- ProfessorsCodingHeist/src/com/professors/java8/lambda/FP05FunctionalProgrammingUsingDistinctAndSorted.java
+++ ProfessorsCodingHeist/src/com/professors/java8/lambda/FP05FunctionalProgrammingUsingDistinctAndSorted.java
@@ -1,5 +1,6 @@
 package com.professors.java8.lambda;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class FP05FunctionalProgrammingUsingDistinctAndSorted {
@@ -13,5 +14,7 @@ public class FP05FunctionalProgrammingUsingDistinctAndSorted {
 		//Prints all the distinct  values
 		numbers.stream().sorted().forEach(System.out::print);
 		numbers.stream().distinct().sorted((num1, num2)-> Integer.compare(num1, num2)).forEach(System.out::println);
+		System.out.println("List of Numbers in Reverse Order: ");
+		numbers.stream().distinct().sorted(Comparator.reverseOrder()).forEach(System.out::println);
 	}
 }
