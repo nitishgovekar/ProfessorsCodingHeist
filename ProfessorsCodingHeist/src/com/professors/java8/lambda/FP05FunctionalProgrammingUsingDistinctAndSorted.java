5f2e4d6f2a742844636836c74fba03cde1c29c58
nitish.govekar <nitishgovekar1992@gmail.com>
2021-10-13 20:15:05 +0530

    Updated code to use distinct and sorted with help of streams

---
+++ ProfessorsCodingHeist/src/com/professors/java8/lambda/FP05FunctionalProgrammingUsingDistinctAndSorted.java
@@ -0,0 +1,17 @@
+package com.professors.java8.lambda;
+
+import java.util.List;
+
+public class FP05FunctionalProgrammingUsingDistinctAndSorted {
+	public static void main(String[] args) {
+
+		List<Integer> numbers = List.of(10,20,7,9,8,5,100,10,100,20,8,9,5);
+		getDistinctNumbersFromList(numbers);
+
+	}
+	private static void getDistinctNumbersFromList(List<Integer> numbers) {
+		//Prints all the distinct  values
+		numbers.stream().sorted().forEach(System.out::print);
+		numbers.stream().distinct().sorted((num1, num2)-> Integer.compare(num1, num2)).forEach(System.out::println);
+	}
+}
